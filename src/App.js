76ecec30783645f0b5aa7143f5be5f24d5d6a6ac
Harsha <84846839+kharshavardhanv@users.noreply.github.com>
2024-03-05 17:21:56 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
+import React, { useState } from 'react';
 import './App.css';
 import Gam from './Gam.js';
 
 function App() {
+  const [n, setN] = useState(null);
+
+  const handleInput = () => {
+    const input = prompt("Enter N value");
+    setN(input);
+  };
+
   return (
     <div className="App">
-      <Gam n={10} num={2}/>
+      <button onClick={handleInput}>Enter N</button>
+      {n && <Gam n={n} num={2} />}
     </div>
   );
 }
 
 export default App;
+
